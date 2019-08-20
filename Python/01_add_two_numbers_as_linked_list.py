c152b0d7cd96c98b346b59c0f76412cafe75b383
Navin Bhaskar <navin.bhaskar.5@gmail.com>
2019-08-20 08:45:55 +0530

    Added second solution

--- Python/01_add_two_numbers_as_linked_list.py
+++ Python/01_add_two_numbers_as_linked_list.py
@@ -1,5 +1,6 @@
 "                       Shree Krishnaya Namaha                  "
 
+import sys
 class ListNode(object):
   def __init__(self, x):
     self.val = x
@@ -36,26 +37,60 @@ class Solution:
 
     return result 
 
-# l1 = ListNode(2)
-# l1.next = ListNode(4)
-# l1.next.next = ListNode(3)
+class SecondSolution:
+  def addTwoNumbers(self, l1, l2, c = 0):
+    num1 = 0
+    num2 = 0
 
-# l2 = ListNode(5)
-# l2.next = ListNode(6)
-# l2.next.next = ListNode(4)
+    mul = 1
+    while l1 is not None:
+        num1 = num1 + l1.val * mul
+        mul = mul * 10
+        l1 = l1.next
+    
+    mul = 1
+    while l2 is not None:
+        num2 = num2 + l2.val * mul
+        mul = mul * 10
+        l2 = l2.next
+    sum_of_two = num1 + num2
+    result_list = None
+    return_list = None
 
-# result = Solution().addTwoNumbers(l1, l2)
-# while result:
-#     print (result.val),
-#     result = result.next
+    if sum_of_two == 0:
+        return ListNode(0)
+    while sum_of_two > 0:
+        lsb = sum_of_two % 10
+        sum_of_two = sum_of_two // 10
+        if result_list:
+            result_list.next = ListNode(lsb)
+            result_list = result_list.next
+        else:
+            result_list = ListNode(lsb)
+            return_list = result_list
 
+    return return_list
+
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
+result = Solution().addTwoNumbers(l1, l2)
+while result:
+    sys.stdout.write(str(result.val))
+    result = result.next
+print ("")
 l1 = ListNode(9)
 l1.next = ListNode(9)
 
 l2 = ListNode(1)
 
 
-result = Solution().addTwoNumbers(l1, l2)
+result = SecondSolution().addTwoNumbers(l1, l2)
 while result:
-    print (result.val),
+    sys.stdout.write(str(result.val))
     result = result.next
